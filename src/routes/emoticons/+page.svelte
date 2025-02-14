988a04005a6bcd04514321c9d2dd67dbd49aeb9c
Lightsoft123 <stevenfebrianto771@gmail.com>
2025-02-14 23:42:36 +0700

    Init shadcn and tailwindcss lib

---
+++ src/routes/emoticons/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Button from "$lib/components/ui/button/button.svelte";
+	import { emoticons } from "./sample-data.ts";
+
+</script>
+
+<div>Emoticons</div>
+<div class="flex w-auto gap-4">
+	{#each emoticons as item}
+		<Button
+			class="border p-2"
+			onclick={() =>
+				navigator.clipboard
+					.writeText(item)
+					.then(() => {
+						console.log('Copied to clipboard!');
+					})
+					.catch((err) => {
+						console.error('Failed to copy: ', err);
+					})}
+		>
+			{item}
+		</Button>
+	{/each}
+</div>
