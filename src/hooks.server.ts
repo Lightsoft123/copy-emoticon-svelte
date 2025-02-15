2956ff5d32d91f40dde2940cc0e3531bfe6a257e
Lightsoft123 <stevenfebrianto771@gmail.com>
2025-02-16 00:21:38 +0700

    feat: make migration run once when the app is started

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,19 @@
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { runMigration } from './db/run-migrations.ts';
 
+let isMigrated = false;
+
 export const handle: Handle = async ({ event, resolve }) => {
-	await runMigration();
+	if (!isMigrated) {
+		await runMigration();
+		isMigrated = true;
+	}
 
 	// if (event.url.pathname.startsWith('/emoticons')) {
 	// 	return new Response('custom response');
 	// }
+
+	if (event.url.pathname === '/') redirect(302, '/emoticons');
 
 	const response = await resolve(event);
 	return response;
