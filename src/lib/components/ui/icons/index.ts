0bf9345107a4c388740f5c152d31698b429cb95b
Lightsoft123 <stevenfebrianto771@gmail.com>
2025-04-16 22:29:04 +0700

    (feat): add lucide svelte icons library

---
+++ src/lib/components/ui/icons/index.ts
@@ -0,0 +1,7 @@
+import ArrowLeft from '@lucide/svelte/icons/arrow-left';
+
+const Icons = {
+	arrowLeft: ArrowLeft
+};
+
+export default Icons;
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
+	import Icons from '$lib/components/ui/icons/index.ts';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import Label from '$lib/components/ui/label/label.svelte';
 
@@ -9,8 +10,10 @@
 	let password: string | undefined = $state();
 </script>
 
-<form method="POST" action="?/register" enctype="multipart/form-data">
-	<Button onclick={() => history.back()}>back</Button>
+<form method="POST" action="?/register" enctype="multipart/form-data" class="grid gap-2">
+	<Button variant="ghost" class="size-8" onclick={() => history.back()}>
+		<Icons.arrowLeft class="size-8" />
+	</Button>
 	<Label for="name">Name</Label>
 	<Input id="name" name="name" bind:value={name} placeholder="name" />
 	<Label for="username">Username</Label>
